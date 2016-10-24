d6955fce1e7ca206be3bfe1a9bd87e694e2e0a17
teak1cd <isenne@sterneschool.org>
2016-10-24 14:47:43 -0700

    Create vJS.0.0.1.js

---
+++ vJS.0.0.1.js
@@ -0,0 +1,18 @@
+var tabs = ["visual","raw","updates"];
+var tcont = ["t1","t2","t3"];
+function l(){
+  window.requestAnimationFrame(l);
+  switch(document.getElementById("fileTab").value.toString()){
+    case "1":
+      alert("open");
+    break;
+    case "2":
+      alert("compile");
+    break;
+    case "3":
+      alert("options");
+    break;
+  }
+  document.getElementById("fileTab").value = "0";
+}
+l();
