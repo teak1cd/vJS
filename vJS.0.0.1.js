d6f1b81052b1ff37f30b45eb10fab5ae525fd62a
teak1cd <isenne@sterneschool.org>
2016-10-25 14:02:54 -0700

    Update vJS.0.0.1.js

--- vJS.0.0.1.js
+++ vJS.0.0.1.js
@@ -1,5 +1,7 @@
+var src="https://www.google.com/search?num=100&site=&source=hp&q=test&oq=test&gs_l=hp.3..0l2j0i131k1l3j0l5.8516.8804.0.8962.5.5.0.0.0.0.162.645.1j4.5.0....0...1c.1.64.hp..0.4.578.0..0i131i155k1.wsl5nPs6jnw"
 var tabs = ["visual","raw","updates"];
 var tcont = ["t1","t2","t3"];
+var frame = document.getElementById("loader");
 function l(){
   window.requestAnimationFrame(l);
   switch(document.getElementById("fileTab").value.toString()){
@@ -7,7 +9,8 @@ function l(){
       alert("open");
     break;
     case "2":
-      alert("compile");
+      var lookup = document.getElementById("topic").value;
+      loader.src=src.replace(/test/gm,lookup);
     break;
     case "3":
       window.close();
